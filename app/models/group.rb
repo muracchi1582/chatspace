d199bbe68196492942f5609bcf62ff6a8fca5f36
muracchi1582 <masa1582nh@yahoo.co.jp>
2020-01-04 11:33:09 +0900

    三項演算子

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if(last_message = message.last).present?
+      if lasst_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
