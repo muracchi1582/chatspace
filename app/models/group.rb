6007199ec001122ff202b0c08c05c3be3d7fa9fa
muracchi1582 <masa1582nh@yahoo.co.jp>
2020-01-04 16:59:58 +0900

    form make

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,8 +5,8 @@ class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def show_last_message
-    if(last_message = message.last).present?
-      if lasst_message.content?
+    if(last_message = messages.last).present?
+      if last_message.content?
         last_message.content
       else
         '画像が投稿されています'
