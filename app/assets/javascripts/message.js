fdb80082b9c43257ca8f0a4c0e860d9eb68c8551
muracchi1582 <masa1582nh@yahoo.co.jp>
2020-01-04 18:50:07 +0900

    hidouki make

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,13 @@
 $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault()
+    $.ajax({
+      url: 取得したリクエストURL,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: 取得したFormData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
