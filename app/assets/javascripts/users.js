4a65ef9f2d217beca7cc9677d4b8c739c4d6e0a7
muracchi1582 <masa1582nh@yahoo.co.jp>
2020-01-05 19:12:37 +0900

    users.js

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,27 @@
+$(function() {
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function(users) {
+        $("#user-search-result").empty();
+
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+      .fail(function() {
+        console.log("失敗です");
+      });
+  });
+});
