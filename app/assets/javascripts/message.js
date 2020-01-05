138b77627fda529a28f926bbddd024b6a5892815
muracchi1582 <masa1582nh@yahoo.co.jp>
2020-01-05 14:24:50 +0900

    message.js make

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,54 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+       `<div class="right__contents--bellow__box" data-message-id=${message.id}>
+            <div class="right__contents--bellow__box-name">
+              ${message.user_name}
+            </div>
+            <div class="right__contents--bellow__box-time">
+              ${message.created_at}
+            </div>
+            <div class="right__contents--bellow__box-message">
+              <p class="right__contents--bellow_-box-message">
+                ${message.content}
+              </p>
+              <img class = "lower-message__image" src="${message.image}">
+            </div>
+        </div>`
+      return html;
+    } else {
+      var html =
+      `<div class="right__contents--bellow__box" data-message-id=${message.id}>
+          <div class="right__contents--bellow__box-name">
+            ${message.user_name}
+          </div>
+          <div class="right__contents--bellow__box-time">
+            ${message.created_at}
+          </div>
+          <div class="right__contents--bellow__box-message">
+            <p class="right__contents--bellow_-box-message">
+              ${message.content}
+            </p>
+          </div>
+      </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
-    e.preventDefault()
-    var formData = new FormDate(this);
+    e.preventDefault();
+    var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      url: url,
+      type: "POST",
       data: formData,  
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
     })
   });
 });
